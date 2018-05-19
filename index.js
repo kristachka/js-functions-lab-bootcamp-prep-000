d02331b8bf90367e5eb862a6c880026a4a47e2b2
Krista <kristalillia@gmail.com>
2018-05-19 03:52:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function happyHolidays() {
-  console.log('Happy holidays!')
+   return 'Happy holidays!'
 }
 
 function happyHolidaysTo(name) {
-  console.log(`Happy holidays, ${name}!`)
+  return `Happy holidays, ${name}!`
 }
 
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
